3e0c2ee1f0250a2b90165e33e0f1893e375310d8
= <=>
2012-04-29 01:24:08 +0800

    1.6.cpp added

---
+++ 1.6.cpp
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+void rotate(int **matrix, int n) {
+	int lt;
+	for (int i = 0; i < n / 2; ++i) {
+		for (int j = i; j < n - 2 * i - 1; ++j) {
+			lt 				= matrix[i][j];
+			matrix[i][j]			= matrix[j][n - 1 - i];
+			matrix[j][n - 1 - i]		= matrix[n - 1 - i][n - 1 - j];
+			matrix[n - 1 - i][n - 1 - j] 	= matrix[n - 1 - j][i];
+			matrix[n - 1 - j][i] 		= lt;
+		}
+	}	
+}
+
+void print_matrix(int **matrix, int n) {
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) 
+			cout << matrix[i][j] << "\t"; 
+
+		cout << endl;
+	}
+
+	cout << endl;
+}
+
+int main() {
+	const int n = 5;
+	int *matrix[n];	
+	for (int i = 0; i < n; ++i) {
+		matrix[i] = new int[n];
+
+		for (int j = 0; j < n; ++j) 
+			matrix[i][j] = rand() % (n * n);
+	}
+		
+	print_matrix(matrix, n);
+	rotate(matrix, n);
+	print_matrix(matrix, n);
+
+	return 0;
+}
