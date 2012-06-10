cff838c3e8a66be04ff9fe5e5200b52247d5c7b2
= <=>
2012-06-10 22:46:48 +0800

    5.6.cpp added

---
+++ 5.6.cpp
@@ -0,0 +1,36 @@
+/////////////////////////////////////////////////////////////////////////////////
+// create two masks: odd_mask records all the bits in odd positions and even_mask
+// records all the bits in even positions; right shift the even_mask and left
+// shift the odd_mask. excluding the odd(even) mask generating procedure, 5 
+// operations are taken
+/////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+template <typename T>
+void print_bin(T val) {
+	for (int i = sizeof(T) * 8 - 1; i >= 0; --i) {
+		(val & (1 << i))? (cout << 1) : (cout << 0);
+	}
+
+	cout << endl;
+}
+
+template <typename T>
+T swap_odd_even(T val) {
+	unsigned int len = sizeof(T) * 8; T odd_mask = 0, even_mask = 0;
+	for (int i = 0; i < len; i += 2) { odd_mask |= (1 << i); }
+	for (int i = 1; i < len; i += 2) { even_mask |= (1 << i); }
+	return ((val & odd_mask) << 1) | ((val & even_mask) >> 1);
+}
+
+int main() {
+	srand(time(0)); int val = rand();
+	print_bin<int>(val);
+	print_bin<int>(swap_odd_even<int>(val));
+
+	return 0;
+}
