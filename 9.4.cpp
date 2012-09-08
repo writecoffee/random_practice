15044f52c39fdb475d8344ac6991600148fbb687
Silao Xu <silao_xu@cs.brown.edu>
2012-09-07 23:17:47 -0400

    9.4.cpp added Fri Sep 7, 11:17 PM

--- 9.4.cpp
+++ 9.4.cpp
@@ -3,23 +3,18 @@
 #include <cassert>
 #include <queue>
 #include <cmath>
+#include <cstring>
 using namespace std;
 
-
+template <typename T>
+struct pq_pair {
+	pq_pair(const T &element, int position) : elem(element), pos(position) { }
+	const T &elem; int pos;
+};
 
 template <typename T>
-void chunk_sort(int disk_size, int mem_size, T *data, int elem_cnt) {
-	assert(data);
-	assert(disk_size > mem_size);
-
-	int elem_sz	= sizeof(*data);
-	int chunk_sz	= ceil(mem_size / elem_sz);
-	int chunk_cnt	= ceil(disk_size / chunk_sz);
-	for (int i = 0; i < chunk_cnt; ++i) {
-		for (int j = 0; j < chunk_sz - 1; ++j) {
-			sort(data + j * chunk_sz, data + (j + 1) * chunk_sz);
-		}
-	}
+bool operator>(const pq_pair<T> &left, const pq_pair<T> &right) {
+	return left.elem > right.elem;
 }
 
 template <typename T>
@@ -31,31 +26,64 @@ T *n_merge_sort(T *data, int elem_cnt, int disk_size, int mem_size, int chunk_sz
 	T *new_space = new T[elem_cnt];
 	int si = 0;
 	// NOTE: just for simulation
-	mem_set(new_space, 0, elem_cnt);
-	c_heap heap();
+	memset(new_space, 0, elem_cnt);
+	priority_queue<pq_pair<T>/**, vector<pq_pair<T> >, greater<pq_pair<T> > **/> heap();
 	int pos_arr[chunk_cnt];
 	memset(pos_arr, 0, chunk_cnt * sizeof(int));
 	for (int i = 0; i < chunk_cnt; ++i) {
-		heap.heappush(make_pair<T, int>(data[i * chunk_sz], i));
+		heap.push(pq_pair<T>(data[i * chunk_sz], i));
 	}
 
 	while (si != elem_cnt) {
-		pair<T, int> min = heap.heappop();
-		new_space[si++] = min.first;
-		pos_arr[min.second]++;
-		if (pos_arr[min.second] + 1 != chunk_sz) {
-			heap.heappush(make_pair<T, int>(data[chunk_sz * min.second + pos_arr[min.second]], min.second));
+		pq_pair<T> min = heap.top(); heap.pop();
+		new_space[si++] = min.elem;
+		pos_arr[min.pos]++;
+		if (pos_arr[min.pos] + 1 != chunk_sz) {
+			heap.push(pq_pair<T>(data[chunk_sz * min.pos + pos_arr[min.pos]], min.pos));
 		}
 	}
 	
 	return new_space;
 }
 
-int test() {
+template <typename T>
+void chunk_sort(int disk_size, int mem_size, T *data, int elem_cnt) {
+	assert(data);
+	assert(disk_size > mem_size);
 
+	int elem_sz		= sizeof(*data);
+	int mem_chk_capacity	= ceil(mem_size / elem_sz);
+	int chunk_sz		= mem_chk_capacity;
+	int dsk_chk_capacity	= ceil(disk_size / elem_sz);
+	int chunk_cnt		= ceil(dsk_chk_capacity / mem_chk_capacity);
+
+	for (int i = 0; i < chunk_cnt; ++i) {
+		for (int j = 0; j < chunk_sz - 1; ++j) {
+			sort(data + j * chunk_sz, data + (j + 1) * chunk_sz);
+		}
+	}
+
+	n_merge_sort<T>(data, elem_cnt, disk_size, mem_size, chunk_sz, elem_sz, chunk_cnt);
+}
+
+
+
+int test() {
+	string *disk = new string[3];
+	disk[0] = string("Line 2: Right! I'll be there.");
+	disk[1] = string("Line 1: Is this a test?");
+	disk[2] = string("Line 3: COME ON!");
+
+	const int elem_cnt	= 3;
+	int disk_sz 		= sizeof(disk) * elem_cnt;
+	int mem_sz 		= sizeof(disk);
+
+	chunk_sort<string>(disk_sz, mem_sz, disk, elem_cnt);
+	cout << "done" << endl;
+	
 }
 
 int main() {
-
+	test();
  	return 0;
 }
