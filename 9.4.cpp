2f9c2b3cfa317147b0eaa3aeba46a4d76316a8ab
writecoffee <writecoffee@gmail.com>
2012-09-22 01:32:46 -0400

    External sort & Merge sort

--- 9.4.cpp
+++ 9.4.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 template <typename T>
 struct pq_pair {
-	pq_pair(const T &element, int position) : elem(element), pos(position) { }
-	const T &elem; int pos;
+	pq_pair(T element, int position) : elem(element), pos(position) { }
+	T elem; int pos;
 };
 
 template <typename T>
@@ -17,29 +17,35 @@ bool operator>(const pq_pair<T> &left, const pq_pair<T> &right) {
 	return left.elem > right.elem;
 }
 
-template <typename T>
-T *n_merge_sort(T *data, int elem_cnt, int disk_size, int mem_size, int chunk_sz, int elem_sz, int chunk_cnt) {
-	assert(mem_size >= chunk_cnt * elem_sz);
+string *n_merge_sort(string *data, int elem_cnt, int disk_size, int mem_size, int chunk_sz, int elem_sz, int chunk_cnt, int lst_chk, int lst_chk_sz) {
+	assert(disk_size >= chunk_cnt * elem_sz);
 	assert(mem_size < disk_size);
 	assert(data);
 	
-	T *new_space = new T[elem_cnt];
-	int si = 0;
-	// NOTE: just for simulation
-	memset(new_space, 0, elem_cnt);
-	priority_queue<pq_pair<T>/**, vector<pq_pair<T> >, greater<pq_pair<T> > **/> heap();
-	int pos_arr[chunk_cnt];
-	memset(pos_arr, 0, chunk_cnt * sizeof(int));
-	for (int i = 0; i < chunk_cnt; ++i) {
-		heap.push(pq_pair<T>(data[i * chunk_sz], i));
-	}
+	string *new_space = new string[elem_cnt];
 
+	// just for simulation
+	priority_queue<pq_pair<string>, vector<pq_pair<string> >, greater<vector<pq_pair<string> >::value_type > > heap;
+
+	// array reserved for recording current reading position for each chunk
+	int pos_arr[chunk_cnt]; memset(pos_arr, 0, chunk_cnt * sizeof(int));
+
+	// iterate the first element in each chunk
+	for (int i = 0; i < chunk_cnt; ++i) { heap.push(pq_pair<string>(data[i * chunk_sz], i)); }
+
+	int si = 0;
 	while (si != elem_cnt) {
-		pq_pair<T> min = heap.top(); heap.pop();
-		new_space[si++] = min.elem;
+		pq_pair<string> min = heap.top(); heap.pop();
+		new_space[si++].append(min.elem);
 		pos_arr[min.pos]++;
-		if (pos_arr[min.pos] + 1 != chunk_sz) {
-			heap.push(pq_pair<T>(data[chunk_sz * min.pos + pos_arr[min.pos]], min.pos));
+
+		if (min.pos == lst_chk) {
+			if (pos_arr[min.pos] < lst_chk_sz) {
+				heap.push(pq_pair<string>(data[chunk_sz * min.pos + pos_arr[min.pos]], min.pos));
+			}
+			continue;
+		} else if (pos_arr[min.pos] < chunk_sz) {
+			heap.push(pq_pair<string>(data[chunk_sz * min.pos + pos_arr[min.pos]], min.pos));
 		}
 	}
 	
@@ -47,40 +53,47 @@ T *n_merge_sort(T *data, int elem_cnt, int disk_size, int mem_size, int chunk_sz
 }
 
 template <typename T>
-void chunk_sort(int disk_size, int mem_size, T *data, int elem_cnt) {
+string *chunk_sort(int disk_size, int mem_sz, T *data, int elem_cnt) {
 	assert(data);
-	assert(disk_size > mem_size);
+	assert(disk_size > mem_sz);
 
 	int elem_sz		= sizeof(*data);
-	int mem_chk_capacity	= ceil(mem_size / elem_sz);
+	int mem_chk_capacity	= ceil((double)mem_sz / elem_sz);
 	int chunk_sz		= mem_chk_capacity;
-	int dsk_chk_capacity	= ceil(disk_size / elem_sz);
-	int chunk_cnt		= ceil(dsk_chk_capacity / mem_chk_capacity);
+	int dsk_chk_capacity	= ceil((double)disk_size / elem_sz);
+	int chunk_cnt		= ceil((double)dsk_chk_capacity / mem_chk_capacity);
+	int lst_chk_sz		= elem_cnt - dsk_chk_capacity / mem_chk_capacity * chunk_sz;
+	int t			= dsk_chk_capacity / mem_chk_capacity;
 
-	for (int i = 0; i < chunk_cnt; ++i) {
+	for (int i = 0; i < chunk_cnt - 1; ++i) {
 		for (int j = 0; j < chunk_sz - 1; ++j) {
-			sort(data + j * chunk_sz, data + (j + 1) * chunk_sz);
+			sort(data + i * chunk_sz, data + (i + 1) * chunk_sz);
 		}
 	}
 
-	n_merge_sort<T>(data, elem_cnt, disk_size, mem_size, chunk_sz, elem_sz, chunk_cnt);
+	if (lst_chk_sz == 0) {
+		return n_merge_sort(data, elem_cnt, disk_size, mem_sz, chunk_sz, elem_sz, chunk_cnt, t + 1, chunk_sz);
+	} else {
+		sort(data + t * chunk_sz, data + t * chunk_sz + lst_chk_sz);
+		return n_merge_sort(data, elem_cnt, disk_size, mem_sz, chunk_sz, elem_sz, chunk_cnt, t, lst_chk_sz);
+	}
 }
 
-
-
 int test() {
-	string *disk = new string[3];
+	const int elem_cnt	= 5;
+
+	string *disk = new string[elem_cnt];
 	disk[0] = string("Line 2: Right! I'll be there.");
 	disk[1] = string("Line 1: Is this a test?");
 	disk[2] = string("Line 3: COME ON!");
+	disk[3] = string("Line 4: Baby!");
+	disk[4] = string("Line 5: Good purchase");
 
-	const int elem_cnt	= 3;
-	int disk_sz 		= sizeof(disk) * elem_cnt;
-	int mem_sz 		= sizeof(disk);
+	int disk_sz 		= sizeof(*disk) * elem_cnt;
+	int mem_sz 		= sizeof(*disk) * 2;
 
-	chunk_sort<string>(disk_sz, mem_sz, disk, elem_cnt);
-	cout << "done" << endl;
-	
+	string *result = chunk_sort<string>(disk_sz, mem_sz, disk, elem_cnt);
+	for (int i = 0; i < elem_cnt; ++i) { cout << result[i] << endl; }
 }
 
 int main() {
